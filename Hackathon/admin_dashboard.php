2798d15fbe97e7d69fa49e57537a6283a73cd501
Jayson Reyes <marklawrencezalamea@gmail.com>
2022-10-30 12:17:33 +0800

    commit

--- Hackathon/admin_dashboard.php
+++ Hackathon/admin_dashboard.php
@@ -48,78 +48,84 @@
       <li>
         <div class="iocn-link">
           <a href="#">
-            <i class='bx bx-book-alt' ></i>
-            <span class="link_name">FLIPINO OVERSEAS & WORKERS</span>
+            <i class='bx bx-collection'></i>
+            <span class="link_name">Filipino Overseas & Workers</span>
           </a>
           <i class='bx bxs-chevron-down arrow' ></i>
         </div>
         <ul class="sub-menu">
-          <li><a class="link_name" href="#">FLIPINO OVERSEAS & WORKERS</a></li>
+          <li><a class="link_name" href="#">Filipino Overseas & Workers</a></li>
           <li><a href="#">HOUSEHOLDS</a></li>
           <li><a href="#">TOTAL POPULATION</a></li>
-          <li><a href="#">Card Design</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+            <li><a href="#">Employed Members of the Labor Office</a></li>
+            <li><a href="#">Unemployed Members of the Labor Office</a></li>
+            <li><a href="#">Under Employed Workers</a></li>
         </ul>
       </li>
+
       <li>
-        <a href="#">
-          <i class='bx bx-pie-chart-alt-2' ></i>
-          <span class="link_name">Analytics</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Analytics</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-line-chart' ></i>
-          <span class="link_name">Chart</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Chart</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="iocn-link">
+      <div class="iocn-link">
           <a href="#">
-            <i class='bx bx-plug' ></i>
-            <span class="link_name">Plugins</span>
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">PWD, Senior Citizen, Solo Parent</span>
           </a>
           <i class='bx bxs-chevron-down arrow' ></i>
         </div>
         <ul class="sub-menu">
-          <li><a class="link_name" href="#">Plugins</a></li>
-          <li><a href="#">UI Face</a></li>
-          <li><a href="#">Pigments</a></li>
-          <li><a href="#">Box Icons</a></li>
+          <li><a class="link_name" href="#">PWD, Senior Citizen, Solo Parent</a></li>
+          <li><a href="#">HOUSEHOLDS</a></li>
+          <li><a href="#">TOTAL POPULATION</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+            <li><a href="#">Employed Members of the Labor Office</a></li>
+            <li><a href="#">Unemployed Members of the Labor Office</a></li>
+            <li><a href="#">Under Employed Workers</a></li>
         </ul>
       </li>
+    
       <li>
-        <a href="#">
-          <i class='bx bx-compass' ></i>
-          <span class="link_name">Explore</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Explore</a></li>
+      <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Agriculture</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Agriculture</a></li>
+          <li><a href="#">HOUSEHOLDS</a></li>
+          <li><a href="#">TOTAL POPULATION</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+            <li><a href="#">Employed Members of the Labor Office</a></li>
+            <li><a href="#">Unemployed Members of the Labor Office</a></li>
+            <li><a href="#">Under Employed Workers</a></li>
         </ul>
       </li>
+
       <li>
-        <a href="#">
-          <i class='bx bx-history'></i>
-          <span class="link_name">History</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">History</a></li>
+      <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Agriculture</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Agriculture</a></li>
+          <li><a href="#">HOUSEHOLDS</a></li>
+          <li><a href="#">TOTAL POPULATION</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+          <li><a href="#">FILIPINO OVERSEAS</a></li>
+            <li><a href="#">Employed Members of the Labor Office</a></li>
+            <li><a href="#">Unemployed Members of the Labor Office</a></li>
+            <li><a href="#">Under Employed Workers</a></li>
         </ul>
       </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-cog' ></i>
-          <span class="link_name">Setting</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Setting</a></li>
-        </ul>
-      </li>
+     
+     
       <li>
     <div class="profile-details">
       <div class="profile-content">
